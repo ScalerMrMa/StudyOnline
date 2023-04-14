4fd34acabcc710c260ef129eb19069693dc23146
ScalerMrMa <813777246@qq.com>
2023-04-14 23:20:27 +0800

    第三次提交 完善工程结构

---
+++ xuecheng-plus-parent/src/main/java/com/it/xuecheng/XuechengPlusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.it.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusProjectApplication.class, args);
+    }
+
+}
---
+++ xuecheng-plus-parent/src/test/java/com/it/xuecheng/XuechengPlusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.xuecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
